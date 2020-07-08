c2d1a0afd4cf09c1528ef0ed095a394ae660f4f5
Kishan Gohil <gohilkishan159@gmail.com>
2020-07-08 20:59:34 +0530

    Updates

---
+++ precache-manifest.f4d2f825b8aa7c3e8baae78d9517b6b9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c23810d04a6d9d5bf0f01caa132d4b9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "73e4e22730fbedd7cdee",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "be89b2b64defe1c5f87b",
+    "url": "/Portfolio/static/css/main.0b934966.chunk.css"
+  },
+  {
+    "revision": "73e4e22730fbedd7cdee",
+    "url": "/Portfolio/static/js/2.48349c7a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.48349c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be89b2b64defe1c5f87b",
+    "url": "/Portfolio/static/js/main.51cc92cb.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "8a4c6423c752d15201d40d7cf293e0cd",
+    "url": "/Portfolio/static/media/alf.8a4c6423.png"
+  },
+  {
+    "revision": "3b6445caba67ea8e2da1276312d9a706",
+    "url": "/Portfolio/static/media/coding_bg.3b6445ca.jpg"
+  },
+  {
+    "revision": "deeb777ad709d20554e557e50cb2d408",
+    "url": "/Portfolio/static/media/my_photo.deeb777a.jpg"
+  },
+  {
+    "revision": "4ab47b304f6f7d473cd30fecf89e0a10",
+    "url": "/Portfolio/static/media/pa.4ab47b30.png"
+  },
+  {
+    "revision": "d7d7f23c827965839397fd6c1eff50ae",
+    "url": "/Portfolio/static/media/tabib.d7d7f23c.png"
+  }
+]);
